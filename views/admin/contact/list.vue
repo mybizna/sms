af7cd28753504ec1490b7983e8406f80f8f136b1
Dedan Irungu <dedanirungu@gmail.com>
2023-02-24 11:51:56 +0300

    Changing list.vue from json to using slots

--- views/admin/contact/list.vue
+++ views/admin/contact/list.vue
@@ -13,7 +13,9 @@
             <td>{{ item.name }}</td>
             <td>{{ item.phone }}</td>
             <td>{{ item.ordering }}</td>
-            <td>{{ item.published }}</td>
+            <td class="text-center">
+                <btn-status :status="item.published"></btn-status>
+            </td>
         </template>
     </table-render>
 </template>
--- views/admin/gateway/list.vue
+++ views/admin/gateway/list.vue
@@ -13,7 +13,9 @@
             <td>{{ item.tinametle }}</td>
             <td>{{ item.url }}</td>
             <td>{{ item.ordering }}</td>
-            <td>{{ item.published }}</td>
+            <td class="text-center">
+                <btn-status :status="item.published"></btn-status>
+            </td>
         </template>
 
     </table-render>
