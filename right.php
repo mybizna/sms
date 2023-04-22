ff4f4dc27ddd9f046cca4ca089c63922d54f6dad
Dedan Irungu <dedanirungu@gmail.com>
2023-04-22 15:17:14 +0300

    Update rights

---
+++ right.php
@@ -0,0 +1,43 @@
+<?php
+
+$this->add_right("sms", "blacklist", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "blacklist", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "blacklist", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "blacklist", "staff", view:true, add:true, edit:true);
+$this->add_right("sms", "blacklist", "registered", view:true, add:true);
+$this->add_right("sms", "blacklist", "guest", view:true, );
+
+$this->add_right("sms", "contact", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "contact", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "contact", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "contact", "staff", view:true, add:true, edit:true);
+$this->add_right("sms", "contact", "registered", view:true, add:true);
+$this->add_right("sms", "contact", "guest", view:true, );
+
+$this->add_right("sms", "gateway", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "gateway", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "gateway", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "gateway", "staff", view:true, add:true, edit:true);
+$this->add_right("sms", "gateway", "registered", view:true, add:true);
+$this->add_right("sms", "gateway", "guest", view:true, );
+
+$this->add_right("sms", "outgoing", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "outgoing", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "outgoing", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "outgoing", "staff", view:true, add:true, edit:true);
+$this->add_right("sms", "outgoing", "registered", view:true, add:true);
+$this->add_right("sms", "outgoing", "guest", view:true, );
+
+$this->add_right("sms", "signature", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "signature", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "signature", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "signature", "staff", view:true, add:true, edit:true);
+$this->add_right("sms", "signature", "registered", view:true, add:true);
+$this->add_right("sms", "signature", "guest", view:true, );
+
+$this->add_right("sms", "whitelist", "administrator", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "whitelist", "manager", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "whitelist", "supervisor", view:true, add:true, edit:true, delete:true);
+$this->add_right("sms", "whitelist", "staff", view:true, add:true, edit:true);
+$this->add_right("sms", "whitelist", "registered", view:true, add:true);
+$this->add_right("sms", "whitelist", "guest", view:true, );
