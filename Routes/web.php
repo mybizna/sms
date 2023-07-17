8ed27be319713b3c472591abc55b1439be5e6046
Dedan Irungu <dedanirungu@gmail.com>
2023-07-17 13:58:34 +0300

    Improvement to Outgoing routes

--- Routes/web.php
+++ Routes/web.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get ( 'sms/outgoing/{secretkey}', 'OutgoingController@outgoing')->name('sms_outgoing')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::post ( 'sms/outgoing/{secretkey}', 'OutgoingController@outgoing_update')->name('sms_outgoing')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::post ( 'sms/outgoing/{secretkey}', 'OutgoingController@outgoing_update')->name('sms_outgoing_post')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
