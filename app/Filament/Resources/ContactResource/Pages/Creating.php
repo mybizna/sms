6ba1da024337a2e56ec007716d4773eff7b5076d
Dedan <dedanirungu@gmail.com>
2024-10-13 16:39:54 +0300

    Release 2024.12

---
+++ app/Filament/Resources/ContactResource/Pages/Creating.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Sms\Filament\Resources\ContactResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\CreatingBase;
+
+// Class List that extends ListBase
+class Creating extends CreatingBase
+{
+}
---
+++ app/Filament/Resources/ContactResource/Pages/Editing.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Modules\Sms\Filament\Resources\ContactResource\Pages;
+
+use Modules\Base\Filament\Resources\Pages\EditingBase;
+
+// Class List that extends ListBase
+class Editing extends EditingBase
+{
+}
