9246ff5dba0eb4e1b414f87300045c2f82380b9c
Dedan <dedanirungu@gmail.com>
2025-01-31 20:41:34 +0300

    Remove uncessary routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Sms\Http\Controllers\OutgoingController;
 
-Route::get ( 'sms/outgoing/{secretkey}', 'OutgoingController@outgoing')->name('sms_outgoing')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
-Route::post ( 'sms/outgoing/{secretkey}', 'OutgoingController@outgoing_update')->name('sms_outgoing_post')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::get('sms/outgoing/{secretkey}', [OutgoingController::class, 'outgoing'])->name('sms_outgoing')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::post('sms/outgoing/{secretkey}', [OutgoingController::class, 'outgoing_update'])->name('sms_outgoing_post')->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
